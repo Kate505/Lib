7b28a7d0c63a3f6355248a461d5d58d3fc60f4c0
Kate505 <ksdelgado31@gmail.com>
2022-11-23 23:39:50 -0600

    modified:   JavaScript/dashboard.js 	modified:   JavaScript/javascript.js 	deleted:    css/dashboard.css 	modified:   css/styles.css 	modified:   index.html

--- JavaScript/dashboard.js
+++ JavaScript/dashboard.js
@@ -1,59 +1,5 @@
 /* globals Chart:false, feather:false */
 
-(() => {
-  'use strict'
-
-  feather.replace({ 'aria-hidden': 'true' })
-
-  // Graphs
-  const ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-
-  
-  const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-})()
-
 var xValues = [50,60,70,80,90,100,110,120,130,140,150];
 var yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
@@ -100,5 +46,163 @@ new Chart("myotherChart", {
   },
   options: {
     legend: {display: false}
+  }
+});
+
+
+const labels = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre','Diciembre'];
+
+new Chart("barChart", {
+  type: 'bar',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56, 55, 40,100,150,130,160,200,0],
+      lineTension: 0,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgb(201, 203, 207, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)',
+        'rgba(255, 159, 64)',
+        'rgba(255, 205, 86)',
+        'rgba(75, 192, 192)',
+        'rgba(54, 162, 235)',
+        'rgba(153, 102, 255)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    legend: {display: false},
+    
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    title: {
+      display: true,
+      text: 'Año 2022',
+    }
+  }
+});
+
+
+
+const labels2 = ['Semana 1','Semana 2','Semana 3','Semana 4'];
+
+new Chart("semChart", {
+  type: 'bar',
+  data: {
+    labels: labels2,
+    datasets: [{
+      label: 'My First Dataset',
+      data: [2500,2300,1850,3000],
+      lineTension: 0,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    legend: {display: false},
+    
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    title: {
+      display: true,
+      text: 'Noviembre 2022',
+    }
+  }
+});
+
+
+
+new Chart("myChart", {
+  type: 'line',
+  
+  data:{
+    labels: [
+      'Lunes',
+      'Martes',
+      'Miércoles',
+      'Jueves',
+      'Viernes',
+      'Sábado',
+      'Domingo'
+    ],
+    datasets: [{
+        label: 'Dataset',
+        data: [52,62,85,47,36,95,87,0],
+        fill: false,
+        backgroundColor: 'transparent',
+        lineTension: 0,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgb(201, 203, 207,0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        pointStyle: 'circle',
+        pointRadius: 10,
+        pointHoverRadius: 15
+      }]
+  },
+  options: {
+    legend: {display: false},
+    
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    title: {
+      display: true,
+      text: 'dd/mm/xxxx - dd/mm/xxxxx',
+    }
   }
 });
--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,3 +1,26 @@
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+  {
+    duration: 5000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+});
+
+
 /* global bootstrap: false */
 (() => {
     'use strict'
@@ -7,5 +30,6 @@
     })
   })()
   
+
 
   
